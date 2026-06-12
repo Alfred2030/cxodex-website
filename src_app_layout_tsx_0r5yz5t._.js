9fd5c69efc46e465597c83737ef83e4f07242537
Alfred2030 <165610555+Alfred2030@users.noreply.github.com>
2026-06-12 12:03:01 +0800

    Add files via upload

---
+++ src_app_layout_tsx_0r5yz5t._.js
@@ -0,0 +1,3 @@
+module.exports=[27572,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsx)("html",{lang:"zh-CN",children:(0,b.jsx)("body",{children:a})})},"metadata",0,{title:"CXODEX 企业经营健康诊断",description:"面向 CEO 的企业经营问题诊断工具。"}])},50645,a=>{a.n(a.i(27572))}];
+
+//# sourceMappingURL=src_app_layout_tsx_0r5yz5t._.js.map
